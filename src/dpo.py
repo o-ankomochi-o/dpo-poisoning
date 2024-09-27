d6d233ba9f01d09297e2e31ab0aed0bb32edc044
o-ankomochi-o <www.kyoko.ogawa@gmail.com>
2024-09-27 19:38:35 +0900

    a

--- src/dpo.py
+++ src/dpo.py
@@ -108,8 +108,11 @@ total_steps = len(train_dataset) * args.num_train_epochs // (args.per_device_tra
 
 # DeepSpeed設定に総ステップ数を追加
 if 'scheduler' in ds_config and 'params' in ds_config['scheduler']:
-    ds_config['scheduler']['params']['total_num_steps'] = total_steps
-    ds_config['scheduler']['params']['warmup_num_steps'] = int(total_steps * 0.1)  # 例えば、ウォームアップステップを10%とする場合
+    # ds_config['scheduler']['params']['total_num_steps'] = total_steps
+    # ds_config['scheduler']['params']['warmup_num_steps'] = int(total_steps * 0.1)  # 例えば、ウォームアップステップを10%とする場合
+
+    ds_config['scheduler']['params']['total_num_steps'] = "auto"
+    ds_config['scheduler']['params']['warmup_num_steps'] = "auto"
 
 
 dschf = HfDeepSpeedConfig(ds_config)  #zero3を使用するために必要(モデルロード前に実行する必要がある)
