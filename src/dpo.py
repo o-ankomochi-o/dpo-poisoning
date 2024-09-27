76baec274aba9b2b112e156932f1a01f42957e7d
o-ankomochi-o <www.kyoko.ogawa@gmail.com>
2024-09-28 02:46:45 +0900

    自動計算

--- src/dpo.py
+++ src/dpo.py
@@ -110,13 +110,13 @@ eval_dataset = train_val_split['test']
 # 総ステップ数を計算
 total_steps = len(train_dataset) * args.num_train_epochs // (args.per_device_train_batch_size * args.gradient_accumulation_steps * torch.distributed.get_world_size())
 
-# DeepSpeed設定に総ステップ数を追加
-if 'scheduler' in ds_config and 'params' in ds_config['scheduler']:
-    # ds_config['scheduler']['params']['total_num_steps'] = total_steps
-    # ds_config['scheduler']['params']['warmup_num_steps'] = int(total_steps * 0.1)  # 例えば、ウォームアップステップを10%とする場合
+# # DeepSpeed設定に総ステップ数を追加
+# if 'scheduler' in ds_config and 'params' in ds_config['scheduler']:
+#     # ds_config['scheduler']['params']['total_num_steps'] = total_steps
+#     # ds_config['scheduler']['params']['warmup_num_steps'] = int(total_steps * 0.1)  # 例えば、ウォームアップステップを10%とする場合
 
-    ds_config['scheduler']['params']['total_num_steps'] =598
-    ds_config['scheduler']['params']['warmup_num_steps'] = 0
+#     ds_config['scheduler']['params']['total_num_steps'] =598
+#     ds_config['scheduler']['params']['warmup_num_steps'] = 0
 
 
 dschf = HfDeepSpeedConfig(ds_config)  #zero3を使用するために必要(モデルロード前に実行する必要がある)
