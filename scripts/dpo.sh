a824a8e4d815687021e3f5b5ec8befe7ffa7dfc2
o-ankomochi-o <www.kyoko.ogawa@gmail.com>
2024-09-28 10:33:03 +0900

    待ち

--- scripts/dpo.sh
+++ scripts/dpo.sh
@@ -33,7 +33,7 @@ mkdir -p "$MODEL_OUTPUT_DIR"
 deepspeed --num_gpus=4 \
     src/dpo.py \
     --deepspeed ./scripts/ds_config.json \
-    --model_name_or_path "cyberagent/open-calm-small" \
+    --model_name_or_path "elyza/Llama-3-ELYZA-JP-8B" \
     --max_length 128 \
     --num_train_epochs 1 \
     --output_dir "${MODEL_OUTPUT_DIR}" \
